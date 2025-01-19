f6ec66940748f18b9ea3739b1777836c59c87aa6
Samuel Eli <iamsameli@gmail.com>
2025-01-19 09:23:14 +0200

    update database and seeding

--- seeds/index.js
+++ seeds/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const Review = require('../models/review')
 
 //connect to database
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/wanna-camp';
@@ -24,17 +25,41 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 // create a database that contains 50 random
 // cities and state and gives titles using elements
 const seedDB = async () => {
-	await Campground.deleteMany({});
-	for (let i = 0; i < 200; i++) {
+	await Campground.deleteMany({}); // Clear all campgrounds
+	await Review.deleteMany({}); // Clear all reviews
+
+	// Define the same three comments
+	const reviews = await Promise.all([
+		new Review({
+			author: '67866d92294dd7823b91c7d3',
+			body: 'Beautiful place, great for families',
+			rating: 5,
+		}).save(),
+		new Review({
+			author: '67866d92294dd7823b91c7d3',
+			body: 'Stunning location, but very crowded on weekends',
+			rating: 4,
+		}).save(),
+		new Review({
+			author: '67866d92294dd7823b91c7d3',
+			body: 'Highly recommended, we loved it!!',
+			rating: 4,
+		}).save(),
+	]);
+
+	// Seed campgrounds
+	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
-		const price = Math.floor(Math.random() * 20) + 10;
 		const city = cities[random1000];
+		const price = Math.floor(Math.random() * 20) + 10;
+
 		const camp = new Campground({
-			author: '677aaa7d292f2ba824a62ca6',
-			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			author: '67866d92294dd7823b91c7d3', // Production
+			// author: '678ca3978aea078d40b0565f', // Development
+			location: `${city.city}, ${city.state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
 			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sequi voluptatem doloremque tempore quos aperiam vel nesciunt fuga, nihil ipsam! Excepturi et cum fugit eveniet id a autem ad tempora!',
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sequi voluptatem doloremque tempore quos aperiam vel nesciunt fuga, nihil ipsam!',
 			price,
 			geometry: {
 				type: 'Point',
@@ -45,23 +70,21 @@ const seedDB = async () => {
 					url: 'https://res.cloudinary.com/dibf6qmj9/image/upload/v1737261595/glen-jackson-mzZVGFfMOkA-unsplash_oooqwc.jpg',
 					filename: 'WannaCamp/campgroundpic1',
 				},
-				{
-					url: 'https://res.cloudinary.com/dibf6qmj9/image/upload/v1737261595/wei-pan-Ta0A1miYZKc-unsplash_htob4j.jpg',
-					filename: 'WannaCamp/campgroundpic2',
-				},
-				{
-					url: 'https://res.cloudinary.com/dibf6qmj9/image/upload/v1737261595/kevin-ianeselli-ebnlHkqfUHY-unsplash_hvbp0u.jpg',
-					filename: 'WannaCamp/campgroundpic3',
-				},
-				{
-					url: 'https://res.cloudinary.com/dibf6qmj9/image/upload/v1737261595/andreas-ronningen-i9FLJwYhVQs-unsplash_d3wlqw.jpg',
-					filename: 'WannaCamp/campgroundpic4',
-				},
 			],
+			reviews: reviews.map(review => review._id), // Reference review IDs
 		});
+
 		await camp.save();
 	}
+
+	console.log('Database seeded successfully!');
 };
+
+// Run seeding and close the database
+seedDB().then(() => {
+	db.close();
+});
+
 
 seedDB().then(() => {
 	db.close();
